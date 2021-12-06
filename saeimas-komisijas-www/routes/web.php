c23bd14f6979b292ded6d7a37abf84baa093c985
Niks Derums <niks.derums@gmail.com>
2021-12-06 22:30:01 +0200

    Add basic custom authentification

--- saeimas-komisijas-www/routes/web.php
+++ saeimas-komisijas-www/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::get('dashboard', [UserController::class, 'dashboard']); 
+Route::get('login', [UserController::class, 'index'])->name('login');
+Route::post('custom-login', [UserController::class, 'customLogin'])->name('login.custom'); 
+Route::get('registration', [UserController::class, 'registration'])->name('register-user');
+Route::post('custom-registration', [UserController::class, 'customRegistration'])->name('register.custom'); 
+Route::get('signout', [UserController::class, 'signOut'])->name('signout');
