0b85ed0777938eafea7f138fa164c6db89270bab
Niks Derums <niks.derums@gmail.com>
2021-12-16 22:59:17 +0200

    Add Email model

--- saeimas-komisijas-www/database/migrations/2014_10_12_000000_create_users_table.php
+++ saeimas-komisijas-www/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('email_verification_token', 15)->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
---
+++ saeimas-komisijas-www/database/migrations/2021_12_16_132516_create_emails_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('emails', function (Blueprint $table) {
+            $table->id();
+            $table->string('email_address', 255);
+            $table->date('email_verification_time');
+            $table->string('email_verification_token', 15);
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('emails');
+    }
+}
