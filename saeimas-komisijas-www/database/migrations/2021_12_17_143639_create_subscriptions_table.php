4b57f23b41e0e36f553db99c7eaa9c9911efbdc6
Niks Derums <niks.derums@gmail.com>
2021-12-17 17:10:56 +0200

    Add Subscriptions table

---
+++ saeimas-komisijas-www/database/migrations/2021_12_17_143639_create_subscriptions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubscriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('email_id')->unsigned();
+            $table->foreign('email_id')->references('id')->on('emails');
+            $table->integer('commission_id')->unsigned();
+            $table->foreign('commission_id')->references('id')->on('commissions');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+}
