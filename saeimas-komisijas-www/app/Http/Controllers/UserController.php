22449014b268ce13c5891708c39b9a43c5115d77
Niks Derums <niks.derums@gmail.com>
2022-01-02 09:54:53 +0200

    Add autoconfirm of first e-mail address

--- saeimas-komisijas-www/app/Http/Controllers/UserController.php
+++ saeimas-komisijas-www/app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Hash;
 use Session;
 use App\Models\User;
+use App\Models\Email;
 use Illuminate\Support\Facades\Auth;
 use Mail;
 
@@ -49,23 +50,29 @@ class UserController extends Controller // Based on tutorial published here: htt
         $data = $request->all();
 
         $emailConfirmationToken = uniqid();
-        $check = User::create([
+        $newUser = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
             'email_verification_token' => $emailConfirmationToken
         ]);
+
+        $email = new Email();
+        $email->email_address = $data['email'];
+        $email->email_verification_token = $emailConfirmationToken;
+        $email->user_id = $newUser->id;
+        $email->save();
         
         $to_name = $data['name'];
         $to_email = $data['email'];
         $emailContent = array('name'=>$data['name'], 'body' => 'Jūs esat reģistrējies sistēmā "Seko Saeimai". Ja šo darbību tik tiešām veicāt jūs, lūdzu, apstipriniet to, atverot šo saiti: http://localhost:8000/confirm-email?address='.$data['email'].'&token='.$emailConfirmationToken);
 
         Mail::send("emails.mail", $emailContent, function($message) use ($to_name, $to_email) {
-            $message->to($to_email, $to_name)->subject("You have signed in");
+            $message->to($to_email, $to_name)->subject("Apstipriniet savu reģistrāciju 'Seko Saeimai'!");
             $message->from("seko.saeimai@gmail.com","Seko Saeimai");
         });
         
-        return redirect("subscriptions");
+        return redirect("/");
     }    
 
     public function confirmUserEmailAddress(Request $request) {
@@ -73,6 +80,12 @@ class UserController extends Controller // Based on tutorial published here: htt
         if (!empty($user)) {
             $user->email_verified_at = date('Y-m-d H:i:s');
             $user->save();
+
+            $email = Email::where('email_address', $user->email)->where('user_id', $user->id)->where('email_verification_token', $request->get('token'))->first();
+            if (!empty($email)) {
+                $email->email_verification_time = date('Y-m-d H:i:s');
+                $email->save();
+            }
             return redirect("/")->withSuccess('Lietotāja '.$user->email.' e-pasta adrese veiksmīgi apstiprināta.');
         } else {
             return redirect("/")->withErrors('Notika kļūda. Šāds lietotājs neeksistē, šis lietotājs jau ir apstiprinājis savu e-pasta adresi vai arī e-pasta adreses apstiprināšanas saite nav pareiza.');
